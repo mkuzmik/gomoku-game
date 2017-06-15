e330e35eba94c8e1ce8f4b3e59815343c151c554
mkuzmik <mateusz.kuzmik1@gmail.com>
2017-06-15 22:19:01 +0200

    random position generator added

--- Ai.hs
+++ Ai.hs
@@ -1,7 +1,14 @@
 module Ai (makeMove) where
 
 import Gomoku
+import System.Random
+import System.IO.Unsafe
 
--- function that takes board and returns move (x and y in range 1 -:- 19)
+-- function that takes board and returns optimal move (x and y in range 1 -:- 19)
 makeMove :: Board -> Field -> (Int,Int)
-makeMove _ _ = (1,1)
+-- sample return (only to test main function)
+makeMove _ _ = getRandomPosition 1 19
+
+-- two numbers in touple from range
+getRandomPosition :: Int -> Int -> (Int, Int)
+getRandomPosition fir sec = (unsafePerformIO ( getStdRandom ( randomR(fir,sec))),unsafePerformIO ( getStdRandom ( randomR(fir,sec))))
